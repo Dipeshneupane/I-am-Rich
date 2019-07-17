e7287a7739aa10a9f318865d473b2ee13a3ecb56
Dipesh Neupane <dipeshneupane2055@gmail.com>
2019-07-17 15:38:05 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+            title: Text('I am rich'), backgroundColor: Colors.blueGrey[900]),
+        body: Center(
+          child: Image(
+            image:
+                AssetImage('img/Diamond.png'), 
+          ),
+        ),
+      ),
+    ),
+  );
+}
